b14814b14d6fdaf20db349cd4f8462503b522b5c
Harshika-Coder <chaurasiaharshika2612@gmail.com>
2026-06-02 20:50:50 +0530

    Initial Commit

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,15 @@
 import "./style.css";
-import homeLogo from "./assets/Home-logo.png";
+// removed unused/missing home logo import
 
 function Navbar() {
   return (
     <div id="header">
       <nav>
-        <div className="right">Harshika's Portfolio</div>
-        <div>
-          <img src={homeLogo} alt="Home" style={{ width: "50px" }} />
+        <div className="right">
+          <a href="http://localhost:3000">Harshika's Portfolio</a>
         </div>
         <div className="left">
           <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
             <li>
               <a href="#about">About</a>
             </li>
--- src/Recommendation.jsx
+++ src/Recommendation.jsx
@@ -4,18 +4,6 @@ function Recommendations() {
     <div id="recommendations">
       <h2>Recommendations</h2>
       <ul id="recommendation-list">
-        <li>
-          "Harshika is a thoughtful developer who blends creativity with logic."
-          – Peer A
-        </li>
-        <li>
-          "Her donation form project was impactful and beautifully designed." –
-          Peer B
-        </li>
-        <li>
-          "Harshika brings creativity and clarity to every project, making
-          complex ideas feel simple and inspiring." – Peer C
-        </li>
       </ul>
       <hr />
     </div>
